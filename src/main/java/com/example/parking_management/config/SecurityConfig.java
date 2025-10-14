781ef0dbbb0c0757b6ffdfb55ae92f2158a3fa72
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-10-14 15:30:11 -0500

    Se corrigio el dominoo de la app

--- src/main/java/com/example/parking_management/config/SecurityConfig.java
+++ src/main/java/com/example/parking_management/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173/")); // Your frontend origin
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173/, https://aplicacion-de-gestion-para-parqueadero.vercel.app")); // Your frontend origin
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
